c0632947479330445a4e6e4ba1cc80707cf01408
James Dimacali <frostbitezebra421@gmail.com>
2023-12-14 11:26:48 -0800

    Installed pg, aws-S3 upload provider and updated the config files

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env("DATABASE_CLIENT", "postgres");
 
   const connections = {
     mysql: {
@@ -55,25 +55,13 @@ module.exports = ({ env }) => {
     },
     postgres: {
       connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST", "127.0.0.1"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: env.bool("DATABASE_SSL", false) && {
-          key: env("DATABASE_SSL_KEY", undefined),
-          cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
-          capath: env("DATABASE_SSL_CAPATH", undefined),
-          cipher: env("DATABASE_SSL_CIPHER", undefined),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
+        database: env("DATABASE_NAME", ""),
+        user: env("DATABASE_USERNAME", ""),
+        password: env("DATABASE_PASSWORD", ""),
       },
+      useNullAsDefault: true,
       pool: {
         min: env.int("DATABASE_POOL_MIN", 2),
         max: env.int("DATABASE_POOL_MAX", 10),
@@ -115,13 +103,11 @@ module.exports = ({ env }) => {
     },
   };
 
-  
-
   return {
     connection: {
       client,
       ...connections[client],
-      acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
     },
   };
 };
