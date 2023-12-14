e9866a2fb30da1f3a3e4d9257866a90ba88a6424
James Dimacali <frostbitezebra421@gmail.com>
2023-12-14 14:05:45 -0800

    Installed pg, aws-S3 upload provider and updated the config files

--- providers/aws-s3/index.js
+++ providers/aws-s3/index.js
@@ -1,28 +1,28 @@
 // aws-s3 provider
 
-module.exports = {
-  init: (config) => {
-    const s3 = new AWS.S3(config);
+// module.exports = {
+//   init: (config) => {
+//     const s3 = new AWS.S3(config);
 
-    return {
-      async upload(file) {
-        // code to upload file to S3
-      },
+//     return {
+//       async upload(file) {
+//         // code to upload file to S3
+//       },
 
-      async delete(file) {
-        // code to delete file from S3
-      },
+//       async delete(file) {
+//         // code to delete file from S3
+//       },
 
-      async getSignedUrl(file) {
-        const params = {
-          Bucket: config.params.Bucket,
-          Key: file.path,
-          Expires: 60, // URL expiration time in seconds
-        };
+//       async getSignedUrl(file) {
+//         const params = {
+//           Bucket: config.params.Bucket,
+//           Key: file.path,
+//           Expires: 60, // URL expiration time in seconds
+//         };
 
-        const signedUrl = await s3.getSignedUrlPromise("getObject", params);
-        return { url: signedUrl };
-      },
-    };
-  },
-};
+//         const signedUrl = await s3.getSignedUrlPromise("getObject", params);
+//         return { url: signedUrl };
+//       },
+//     };
+//   },
+// };
